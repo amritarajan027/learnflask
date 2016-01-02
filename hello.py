458e40f6597e2e0b2a9eec8a2c2c4e73f7fc1808
stuhari <stuthi.hareendran@gmail.com>
2016-01-02 16:32:32 +0530

    working app with redirect

--- hello.py
+++ hello.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World Index page"
+
+@app.route("/<message>")
+def show_message(message):
+	return "Hello World ! %s"%message
+
+"""@app.route("/<path:p>")
+def zzz():
+	return redirect('/')"""
+
+@app.errorhandler(404) 
+def page_not_found(e): 
+	# your processing here return result
+	return redirect('/')
 
 if __name__ == "__main__":
     app.run()
